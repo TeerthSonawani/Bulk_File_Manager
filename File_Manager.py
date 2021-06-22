12369600b945f4c6c1a42d53c2dda81e973e9d8b
TeerthSonawani <82924733+TeerthSonawani@users.noreply.github.com>
2021-06-22 20:57:45 +0530

    Add files via upload

---
+++ File_Manager.py
@@ -0,0 +1,65 @@
+import os
+
+operation = input('''
+choose an operation-1)Rename
+                    2)Delete
+                    3)Create
+>>>''')
+if operation == '1':
+    Number = 0
+    Stuff = os.listdir()
+    Name = input('what should the files be renamed-')
+    Extension = input('File Extension-')
+    for lmao in Stuff:
+            if lmao == 'File_Manager.py':
+                pass
+            else:
+                os.rename(lmao,f'{Name}{Number}{Extension}')
+                Number += 1
+if operation == '3':
+    Number = 0
+    Amount = int(input('amount of files to be made-'))
+    Name = input('what should the files be named-')
+    Extension = input('File Extension-')
+    for lmao in range(Amount):
+        with open(f'{Name} {Number}{Extension}', 'w') as fp:
+            pass
+        Number +=1
+if operation == '2':
+    what = input('''
+Please Select an option-1)delete using extension
+                        2)delete using name
+                        3)delete all files in the directory
+''')
+    if what == '1':
+        ext = input('Which Extension-')
+        Stuff = os.listdir()
+        for items in Stuff:
+            nameee = os.path.splitext(items)
+            if nameee[1] == f'{ext}':
+                os.remove(items)
+    if what == '2':
+        Number = 0
+        Stuff = os.listdir()
+        namee = input('what name-')
+        ext = input('Extension -')
+        for items in Stuff:
+            Stuff = os.listdir()
+            if items == f'{namee} {Number}{ext}':
+                os.remove(items)
+                Number += 1
+            else:
+                Number += 1
+    if what == '3':
+        Number = 0
+        Stuff = os.listdir()
+        for items in Stuff:
+            if items == 'File_Manager.py':
+                pass
+            else:
+                os.remove(items)
+            Number += 1
+            
+        
+    
+    
